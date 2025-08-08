ba56ea6ab91cbd0f4db38986bb27070dc5dbcc54
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-08-08 22:32:52 +0200

    chore: initial commit

---
+++ packages/design-system/src/theme/color.stylex.ts
@@ -0,0 +1,3 @@
+import stylex from "@bella/stylex";
+
+export const color = stylex.defineVars({ primary: stylex.types.color("#efefef") });
---
+++ tooling/eslint-config/markdown.js
@@ -0,0 +1,9 @@
+import tseslint from "typescript-eslint";
+import markdown from "@eslint/markdown";
+
+export default tseslint.config(markdown.configs.recommended, {
+	language: "markdown/gfm",
+	languageOptions: { frontmatter: "yaml" },
+	name: "markdown overrides",
+	rules: { "markdown/no-bare-urls": "error", "markdown/no-duplicate-headings": "error", "markdown/no-html": "error" },
+});
