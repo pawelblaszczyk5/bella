563953ee555ac5d4ef40b6ce3d4b75bccbab7709
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-08-13 18:39:40 +0200

    fix: properly verify IDs

--- packages/id-generator/src/effect.ts
+++ packages/id-generator/src/effect.ts
@@ -1,6 +1,14 @@
-import { Effect } from "effect";
+import { Effect, Schema } from "effect";
 
 import { generateId, verifyId } from "#src/promise.js";
+
+export class IdVerificationFailed extends Schema.TaggedError<IdVerificationFailed>(
+	"@bella/id-generator/IdVerificationFailed",
+)("IdVerificationFailed", { id: Schema.String }) {
+	override get message() {
+		return `ID verification failed for id "${this.id}"`;
+	}
+}
 
 export class IdGenerator extends Effect.Service<IdGenerator>()("@bella/id-generator/IdGenerator", {
 	dependencies: [],
@@ -9,7 +17,16 @@ export class IdGenerator extends Effect.Service<IdGenerator>()("@bella/id-genera
 			return yield* Effect.tryPromise(async () => generateId()).pipe(Effect.orDie);
 		}),
 		verify: Effect.fn(function* (id: string) {
-			return yield* Effect.tryPromise(async () => verifyId(id)).pipe(Effect.orDie);
+			return yield* Effect.tryPromise(async () => verifyId(id)).pipe(
+				Effect.orDie,
+				Effect.flatMap((isVerified) => {
+					if (isVerified) {
+						return Effect.void;
+					}
+
+					return Effect.fail(new IdVerificationFailed({ id }));
+				}),
+			);
 		}),
 	},
 }) {}
