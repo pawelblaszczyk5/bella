7cfd7b2672a12560fc9a8c25144076d575c2c527
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-08-15 17:22:06 +0200

    refactor: change animation tokens keys

--- packages/design-system/src/theme/animation.stylex.ts
+++ packages/design-system/src/theme/animation.stylex.ts
@@ -1,12 +1,13 @@
 import stylex from "@bella/stylex";
 
 export const duration = stylex.defineVars({
-	x1: stylex.types.time("50ms"),
-	x2: stylex.types.time("100ms"),
-	x3: stylex.types.time("150ms"),
-	x4: stylex.types.time("200ms"),
-	x5: stylex.types.time("300ms"),
-	x6: stylex.types.time("500ms"),
-	x7: stylex.types.time("700ms"),
-	x8: stylex.types.time("1000ms"),
+	1: stylex.types.time("50ms"),
+	2: stylex.types.time("100ms"),
+	3: stylex.types.time("150ms"),
+	4: stylex.types.time("200ms"),
+	5: stylex.types.time("300ms"),
+	6: stylex.types.time("500ms"),
+	7: stylex.types.time("700ms"),
+	8: stylex.types.time("1000ms"),
+	9: stylex.types.time("1500ms"),
 } as const);
