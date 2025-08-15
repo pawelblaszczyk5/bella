c6346893c4aaf61beb3cb390ceea311b35d29f46
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-08-15 13:10:46 +0200

    chore: remove rule in eslint config copied by mistake

--- packages/assert/eslint.config.js
+++ packages/assert/eslint.config.js
@@ -5,5 +5,4 @@ import core from "@bella/eslint-config/core";
 export default defineConfig(
 	{ languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } } },
 	{ extends: [core], files: ["**/*.{ts,tsx,js,jsx}"] },
-	{ files: ["src/mod.ts"], rules: { "import-x/no-default-export": "off" } },
 );
