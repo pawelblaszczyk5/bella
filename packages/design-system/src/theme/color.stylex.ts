f2f907c4c02f7909130795d32fa817d8753b1f29
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-08-17 14:25:19 +0200

    feat: add additional semantic colors

--- packages/design-system/src/theme/color.stylex.ts
+++ packages/design-system/src/theme/color.stylex.ts
@@ -15,6 +15,21 @@ export const violet = stylex.defineVars({
 	"12": stylex.types.color("oklch(0.3127 0.0975 286.76)"),
 });
 
+export const violetDark = stylex.createTheme(violet, {
+	"1": stylex.types.color("oklch(0.1914 0.0261 290.52)"),
+	"2": stylex.types.color("oklch(0.2113 0.0315 299.46)"),
+	"3": stylex.types.color("oklch(0.2709 0.0664 293.92)"),
+	"4": stylex.types.color("oklch(0.3121 0.0929 291.75)"),
+	"5": stylex.types.color("oklch(0.3484 0.0986 291.4)"),
+	"6": stylex.types.color("oklch(0.3897 0.1021 291.82)"),
+	"7": stylex.types.color("oklch(0.4446 0.1109 291.57)"),
+	"8": stylex.types.color("oklch(0.5163 0.1306 290.2)"),
+	"9": stylex.types.color("oklch(0.5418 0.1789 288.1)"),
+	"10": stylex.types.color("oklch(0.5882 0.1695 289.68)"),
+	"11": stylex.types.color("oklch(0.7782 0.1365 293.62)"),
+	"12": stylex.types.color("oklch(0.9116 0.0452 292.61)"),
+});
+
 export const mauve = stylex.defineVars({
 	"1": stylex.types.color("oklch(0.9918 0.0018 321.13)"),
 	"2": stylex.types.color("oklch(0.9832 0.0033 311.22)"),
@@ -28,21 +43,6 @@ export const mauve = stylex.defineVars({
 	"10": stylex.types.color("oklch(0.6106 0.0185 293.07)"),
 	"11": stylex.types.color("oklch(0.5048 0.016 296.08)"),
 	"12": stylex.types.color("oklch(0.2446 0.0134 298.05)"),
-});
-
-export const cyan = stylex.defineVars({
-	"1": stylex.types.color("oklch(0.9921 0.0037 219.25)"),
-	"2": stylex.types.color("oklch(0.9803 0.009 202.89)"),
-	"3": stylex.types.color("oklch(0.9581 0.0265 203.61)"),
-	"4": stylex.types.color("oklch(0.9319 0.041246 204.7361)"),
-	"5": stylex.types.color("oklch(0.8998 0.0538 206.49)"),
-	"6": stylex.types.color("oklch(0.8593 0.066 207.67)"),
-	"7": stylex.types.color("oklch(0.8045 0.0817 209.58)"),
-	"8": stylex.types.color("oklch(0.727 0.1099 211.67)"),
-	"9": stylex.types.color("oklch(0.6609 0.1215 221.49)"),
-	"10": stylex.types.color("oklch(0.6267 0.1141 221.32)"),
-	"11": stylex.types.color("oklch(0.5403 0.127 223.72)"),
-	"12": stylex.types.color("oklch(0.3317 0.0528 218.57)"),
 });
 
 export const mauveDark = stylex.createTheme(mauve, {
@@ -60,19 +60,19 @@ export const mauveDark = stylex.createTheme(mauve, {
 	"12": stylex.types.color("oklch(0.9494 0.0026 286.35)"),
 });
 
-export const violetDark = stylex.createTheme(violet, {
-	"1": stylex.types.color("oklch(0.1914 0.0261 290.52)"),
-	"2": stylex.types.color("oklch(0.2113 0.0315 299.46)"),
-	"3": stylex.types.color("oklch(0.2709 0.0664 293.92)"),
-	"4": stylex.types.color("oklch(0.3121 0.0929 291.75)"),
-	"5": stylex.types.color("oklch(0.3484 0.0986 291.4)"),
-	"6": stylex.types.color("oklch(0.3897 0.1021 291.82)"),
-	"7": stylex.types.color("oklch(0.4446 0.1109 291.57)"),
-	"8": stylex.types.color("oklch(0.5163 0.1306 290.2)"),
-	"9": stylex.types.color("oklch(0.5418 0.1789 288.1)"),
-	"10": stylex.types.color("oklch(0.5882 0.1695 289.68)"),
-	"11": stylex.types.color("oklch(0.7782 0.1365 293.62)"),
-	"12": stylex.types.color("oklch(0.9116 0.0452 292.61)"),
+export const cyan = stylex.defineVars({
+	"1": stylex.types.color("oklch(0.9921 0.0037 219.25)"),
+	"2": stylex.types.color("oklch(0.9803 0.009 202.89)"),
+	"3": stylex.types.color("oklch(0.9581 0.0265 203.61)"),
+	"4": stylex.types.color("oklch(0.9319 0.041246 204.7361)"),
+	"5": stylex.types.color("oklch(0.8998 0.0538 206.49)"),
+	"6": stylex.types.color("oklch(0.8593 0.066 207.67)"),
+	"7": stylex.types.color("oklch(0.8045 0.0817 209.58)"),
+	"8": stylex.types.color("oklch(0.727 0.1099 211.67)"),
+	"9": stylex.types.color("oklch(0.6609 0.1215 221.49)"),
+	"10": stylex.types.color("oklch(0.6267 0.1141 221.32)"),
+	"11": stylex.types.color("oklch(0.5403 0.127 223.72)"),
+	"12": stylex.types.color("oklch(0.3317 0.0528 218.57)"),
 });
 
 export const cyanDark = stylex.createTheme(cyan, {
@@ -88,4 +88,64 @@ export const cyanDark = stylex.createTheme(cyan, {
 	"10": stylex.types.color("oklch(0.698 0.1195 219.12)"),
 	"11": stylex.types.color("oklch(0.7856 0.1154 213.4)"),
 	"12": stylex.types.color("oklch(0.9093 0.0568 211.73)"),
+});
+
+export const tomato = stylex.defineVars({
+	"1": stylex.types.color("oklch(0.9935 0.0031 22.75)"),
+	"2": stylex.types.color("oklch(0.9842 0.0071 31.02)"),
+	"3": stylex.types.color("oklch(0.9544 0.022019 30.8835)"),
+	"4": stylex.types.color("oklch(0.926 0.0471 31.76)"),
+	"5": stylex.types.color("oklch(0.8921 0.0626 31.49)"),
+	"6": stylex.types.color("oklch(0.8524 0.077 32.01)"),
+	"7": stylex.types.color("oklch(0.8028 0.0943 32.17)"),
+	"8": stylex.types.color("oklch(0.7415 0.118231 32.0725)"),
+	"9": stylex.types.color("oklch(0.627 0.1937 33.31)"),
+	"10": stylex.types.color("oklch(0.604 0.1953 33.26)"),
+	"11": stylex.types.color("oklch(0.5665 0.1977 32.81)"),
+	"12": stylex.types.color("oklch(0.3461 0.0798 30.53)"),
+});
+
+export const tomatoDark = stylex.createTheme(tomato, {
+	"1": stylex.types.color("oklch(0.1869 0.0117 19.59)"),
+	"2": stylex.types.color("oklch(0.2077 0.0167 32.02)"),
+	"3": stylex.types.color("oklch(0.2537 0.0544 26.94)"),
+	"4": stylex.types.color("oklch(0.2911 0.087 28.17)"),
+	"5": stylex.types.color("oklch(0.3314 0.0971 28.66)"),
+	"6": stylex.types.color("oklch(0.3801 0.0994 30.01)"),
+	"7": stylex.types.color("oklch(0.4474 0.1068 31.46)"),
+	"8": stylex.types.color("oklch(0.5378 0.1301 32.94)"),
+	"9": stylex.types.color("oklch(0.627 0.1937 33.31)"),
+	"10": stylex.types.color("oklch(0.665 0.1791 34.15)"),
+	"11": stylex.types.color("oklch(0.7843 0.163573 36.0189)"),
+	"12": stylex.types.color("oklch(0.8986 0.0466 31.11)"),
+});
+
+export const amber = stylex.defineVars({
+	"1": stylex.types.color("oklch(0.994 0.0028 84.62)"),
+	"2": stylex.types.color("oklch(0.9862 0.024 100.26)"),
+	"3": stylex.types.color("oklch(0.969 0.0686 100.36)"),
+	"4": stylex.types.color("oklch(0.9454 0.1043 98.19)"),
+	"5": stylex.types.color("oklch(0.9184 0.133285 98.3956)"),
+	"6": stylex.types.color("oklch(0.8812 0.1237 93.24)"),
+	"7": stylex.types.color("oklch(0.8274 0.122 86.18)"),
+	"8": stylex.types.color("oklch(0.7582 0.1404 76.7)"),
+	"9": stylex.types.color("oklch(0.8562 0.1778 81.37)"),
+	"10": stylex.types.color("oklch(0.8315 0.1679 80.94)"),
+	"11": stylex.types.color("oklch(0.5692 0.143894 67.2809)"),
+	"12": stylex.types.color("oklch(0.3519 0.0485 53.74)"),
+});
+
+export const amberDark = stylex.createTheme(amber, {
+	"1": stylex.types.color("oklch(0.1836 0.0126 77.27)"),
+	"2": stylex.types.color("oklch(0.2117 0.0179 78.68)"),
+	"3": stylex.types.color("oklch(0.2575 0.0437 74.56)"),
+	"4": stylex.types.color("oklch(0.2962 0.071462 76.2475)"),
+	"5": stylex.types.color("oklch(0.3369 0.0812 76.41)"),
+	"6": stylex.types.color("oklch(0.3871 0.0784 75.09)"),
+	"7": stylex.types.color("oklch(0.4532 0.0817 74.55)"),
+	"8": stylex.types.color("oklch(0.536 0.0964 73.57)"),
+	"9": stylex.types.color("oklch(0.8562 0.1778 81.37)"),
+	"10": stylex.types.color("oklch(0.9015 0.2065 96.98)"),
+	"11": stylex.types.color("oklch(0.8706 0.1754 85.07)"),
+	"12": stylex.types.color("oklch(0.9349 0.0715 85.99)"),
 });
